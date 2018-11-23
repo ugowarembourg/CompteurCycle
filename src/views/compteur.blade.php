b24425a646e2d9ce79c682c91da52e13dba28157
Ugo <ugo@Debian-Ugo.gpmaurizi.local>
2018-11-23 10:28:20 +0100

    correction

--- src/views/compteur.blade.php
+++ src/views/compteur.blade.php
@@ -4,7 +4,7 @@
     </div>
     <div class="panel-body text-center" data-sensors-id="{{$config->sensor_id}}">
 
-    <h1 class="nb-cycle_{{$config->sensor_id}}">{{$config->nb_cycles}}</h1>
+    <h1 class="nb_cycle_{{$config->sensor_id}}">{{$config->nb_cycles}}</h1>
         <a class="btn btn-primary" style="margin-top: 10px" href="{{url('/widget/infos/'.$config->id.'?view=1')}}">Infos</a><hr>
         <img src="{{$state->image}}" class="image_{{$config->sensor_id}}">
     <div style="margin-bottom: 8px" class="nom_{{$config->sensor_id}}">{{$state->name}}</div>
