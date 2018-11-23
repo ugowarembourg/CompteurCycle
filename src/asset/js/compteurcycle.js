bba741afb590bc87a928f652fd3a4393ec1d7723
Ugo <ugo@Debian-Ugo.gpmaurizi.local>
2018-11-23 10:24:15 +0100

    correction

--- src/asset/js/compteurcycle.js
+++ src/asset/js/compteurcycle.js
@@ -3,7 +3,7 @@ $(function() {
     e.channel('chan-compteurcycle')
         .listen('.UgoWarembourg.Compteurcycle.Events.CompteurCycleEvent', function (e) {
             console.log('CompteurCycle', e);
-            $('.nb_cycle_'+e.config.sensor_id).text(e.config.nb_cycle);
+            $('.nb_cycle_'+e.config.sensor_id).text(e.config.nb_cycles);
             $('.image_'+e.config.sensor_id).attr('src', e.etat.image);
             $('nom_'+e.config.sensor_id).text(e.etat.name);
         })
