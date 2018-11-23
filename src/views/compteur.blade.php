da574be0751786b8c24d365909e136956c2d553f
Ugo <ugo@Debian-Ugo.gpmaurizi.local>
2018-11-23 10:18:52 +0100

    ajout du java script

--- src/views/compteur.blade.php
+++ src/views/compteur.blade.php
@@ -1,12 +1,12 @@
-<div class="panel panel-default roller_shutter_widget roller_shutter">
+<div class="panel panel-default roller_shutter_widget roller_shutter" >
     <div class="panel-heading">
         <h3 class="panel-title">{{$sensor->name}}</h3>
     </div>
-    <div class="panel-body text-center" >
+    <div class="panel-body text-center" data-sensors-id="{{$config->sensor_id}}">
 
-    <div style="font-size: 40px">{{$config->nb_cycles}}</div>
+    <h1 class="nb-cycle_{{$config->sensor_id}}">{{$config->nb_cycles}}</h1>
         <a class="btn btn-primary" style="margin-top: 10px" href="{{url('/widget/infos/'.$config->id.'?view=1')}}">Infos</a><hr>
-        <img src="{{$state->image}}">
-    <div style="margin-bottom: 8px">{{$state->name}}</div>
+        <img src="{{$state->image}}" class="image_{{$config->sensor_id}}">
+    <div style="margin-bottom: 8px" class="nom_{{$config->sensor_id}}">{{$state->name}}</div>
     </div>
 </div>
