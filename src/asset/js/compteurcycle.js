d2b5018bff7a6148294cb227a403c87b50580468
Ugo Warembourg <u.warembourg@groupemaurizi.com>
2019-05-15 17:52:53 +0200

    js

--- src/asset/js/compteurcycle.js
+++ src/asset/js/compteurcycle.js
@@ -1,7 +1,7 @@
 /****************CompteurCycle JS Plugin****************/
 $(function() {
     e.channel('chan-compteurcycle')
-        .listen('.UgoWarembourg.Compteurcycle.Events.CompteurCycleEvent', function (e) {
+        .listen('.UgoWarembourg\\Compteurcycle\\Events\\CompteurCycleEvent', function (e) {
             console.log('CompteurCycle', e);
             $('.nb_cycle_'+e.config.sensor_id).text(e.config.nb_cycles);
             $('.image_'+e.config.sensor_id).attr('src', e.etat.image);
