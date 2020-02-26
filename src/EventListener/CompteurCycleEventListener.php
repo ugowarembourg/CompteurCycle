b285846bf7b81d13469e1cd9a341ed2e6fc0ed30
Ugo Warembourg <u.warembourg@groupemaurizi.com>
2020-02-26 08:31:56 +0100

    compteur

--- src/EventListener/CompteurCycleEventListener.php
+++ src/EventListener/CompteurCycleEventListener.php
@@ -89,12 +89,12 @@ class CompteurCycleEventListener
                 \Log::info('cycle');
                 \Log::info($event->message->getMessage());
                 $nbCycle= $event->message->getMessage();
-                if ($Conf->nb_cycle >= $nbCycle)
+                if ($Conf->nb_cycle > $nbCycle)
                 {
                     $Conf->nb_cycles+=$nbCycle;
                     $compteurcycle->sendCycle($Conf->nb_cycles);
                 }
-                if ($Conf->nb_cycle <= $nbCycle)
+                if ($Conf->nb_cycle < $nbCycle)
                 {
                     $compteurcycle->sendCycle($Conf->nb_cycles);
                 }
